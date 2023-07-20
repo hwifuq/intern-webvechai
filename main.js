f928e1eaeea2505f5b7044c82a1596cbd71513bb
hwifuq <lengochuy6829@gmail.com>
2023-07-20 17:23:17 +0700

    Responsive #7

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ const main = document.querySelector('.main')
 const shortcut = document.querySelector('.short-cut')
 const firstContain = document.querySelector('.top-container')
 const secondContain = document.querySelector('.mid-container') 
-const action = document.querySelector('.sign-in')
+const navigation = document.querySelector('.sub-nav')
+const action = document.querySelector('.action')
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            
+        });
+    });
+});
+
+action.addEventListener('click', ()=>{
+    alert('chua lam ni oi')
+})
+
+navigation.addEventListener('click', ()=>{
+    main.classList.remove('active')
+})
 
 shortcut.addEventListener('click', ()=>{
     main.classList.toggle('active')
